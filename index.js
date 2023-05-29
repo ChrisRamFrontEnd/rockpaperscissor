1cb967986da4d82c7136c389b0d133ec2a21aef5
ChrisRamFrontEnd <pheonixking56885@gmail.com>
2023-05-29 12:49:59 -0400

    Adds singleRound function

--- index.js
+++ index.js
@@ -13,5 +13,15 @@ function compChoice(num){
 }
 
 function singleRound(playerSelection, computerSelection){
-
+    let select = playerSelection.toLowerCase();
+    if(select === computerSelection)
+        return `TIE!! Player selected: ${select}, Computer selecyed: ${computerSelection}`;
+    else if((select === 'rock' && computerSelection === 'scissor') 
+    || (select === 'scissor' && computerSelection === 'paper') 
+    || (select === 'paper' && computerSelection === 'rock'))
+        return `YOU WIN!! ${select} beats ${computerSelection}, nice job!!`;
+    else 
+        return `YOU LOSE!!! ${computerSelection} beats ${select}, better luck next time!`;
 }
+
+console.log(singleRound('ROCK', getComputerChoice()));
