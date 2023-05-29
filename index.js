ce6d63dce346c04ed13d88d452f6d882c3f59a7b
ChrisRamFrontEnd <pheonixking56885@gmail.com>
2023-05-29 13:17:14 -0400

    Add functionality to game func

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
     console.log(choice);
-    return compChoice(+choice);
+    return compChoice(choice);
 }
 
 function compChoice(num){
@@ -15,16 +15,30 @@ function compChoice(num){
 function singleRound(playerSelection, computerSelection){
     let select = playerSelection.toLowerCase();
     if(select === computerSelection)
-        return `TIE!! Player selected: ${select}, Computer selected: ${computerSelection}`;
+        return 2;//`TIE!! Player selected: ${select}, Computer selected: ${computerSelection}`;
     else if((select === 'rock' && computerSelection === 'scissor') 
     || (select === 'scissor' && computerSelection === 'paper') 
     || (select === 'paper' && computerSelection === 'rock'))
-        return `YOU WIN!! ${select} beats ${computerSelection}, nice job!!`;
+        return 1;//`YOU WIN!! ${select} beats ${computerSelection}, nice job!!`;
     else 
-        return `YOU LOSE!!! ${computerSelection} beats ${select}, better luck next time!`;
+        return 0;//`YOU LOSE!!! ${computerSelection} beats ${select}, better luck next time!`;
 }
 
 function game(){
-    let selection = prompt("Please enter one of the three choices, Rock, Paper, or Scissors");
+    let wins = 0, 
+    losses  = 0, 
+    ties = 0;
+    
+    for(let i = 0; i < 5; i++){
+        let selection = prompt("Please enter one of the three choices, Rock, Paper, or Scissors");
+        singleRound(selection, getComputerChoice) === 2 ? ties++ 
+        : singleRound(selection, getComputerChoice) === 1 ? wins++ 
+        : losses++;
+
+        document.getElementById("wins").innerHTML = `Wins: ${wins}`;
+        document.getElementById("losses").innerHTML = `Losses: ${losses}`;
+        document.getElementById("ties").innerHTML = `Ties: ${ties}`;
+    }
+
     
 }
