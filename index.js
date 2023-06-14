6f8afe96cb3ccc20f1fe9fc65036a269edd737c8
ChrisRamFrontEnd <pheonixking56885@gmail.com>
2023-06-14 16:18:22 -0400

    Add functionality of game

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let round = 0, wins = 0, losses = 0;
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
     return compChoice(choice);
@@ -12,22 +13,84 @@ function compChoice(num){
 }
 
 function singleRound(playerSelection, computerSelection){
-    let select = playerSelection.toLowerCase();
-    if(select === computerSelection)
-        console.log('Tie');
-    else if((select === 'rock' && computerSelection === 'scissor') 
-    || (select === 'scissor' && computerSelection === 'paper') 
-    || (select === 'paper' && computerSelection === 'rock'))
-        console.log('Win');
-    else if(!(select === 'rock' && computerSelection === 'scissor') 
-    || !(select === 'scissor' && computerSelection === 'paper') 
-    || !(select === 'paper' && computerSelection === 'rock'))
-        console.log('Lose');
+    const container = document.querySelector('.content');
+    if(playerSelection === computerSelection){
+        const tie = document.createElement('div');
+        tie.classList.add('round');
+        round++;
+        tie.textContent = `Round: ${round}, Tie.`;
+        container.appendChild(tie);
+    }
+    else if((playerSelection === 'rock' && computerSelection === 'scissor') 
+    || (playerSelection === 'scissor' && computerSelection === 'paper') 
+    || (playerSelection === 'paper' && computerSelection === 'rock')){
+        const win = document.createElement('div');
+        win.classList.add('round');
+        round++;
+        wins++;
+        win.textContent = `Round: ${round}, Win.`;
+        container.appendChild(win);
+    }
+    else if(!(playerSelection === 'rock' && computerSelection === 'scissor') 
+    || !(playerSelection === 'scissor' && computerSelection === 'paper') 
+    || !(playerSelection === 'paper' && computerSelection === 'rock')){
+        const lose = document.createElement('div');
+        lose.classList.add('round');
+        round++;
+        losses++;
+        lose.textContent = `Round: ${round}, Lose.`;
+        container.appendChild(lose);
+    }
+
+    if(round === 5){
+        if(wins > losses){
+            const endMessage = document.createElement('div');
+            endMessage.textContent = 'You have beat the Computer!';
+            endMessage.style.cssText = 'color: green';
+            container.appendChild(endMessage);
+        }
+        else if(wins === losses){
+            const endMessage = document.createElement('div');
+            endMessage.textContent = 'You have Tied with the computer, Try again!';
+            endMessage.style.cssText = 'color: blue';
+            container.appendChild(endMessage);
+        }
+        else{
+            const endMessage = document.createElement('div');
+            endMessage.textContent = 'You have Lost to the Computer, better luck next time!';
+            endMessage.style.cssText = 'color: red';
+            container.appendChild(endMessage);
+        }
+    }
+    if(round > 5){
+        resets();
+    }
 }
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) =>{
-    button.addEventListener('click', singleRound(button.id, compChoice(getComputerChoice())));
+function resets(){
+    const container = document.querySelector('.content');
+    while(container.children.length > 0)
+        container.removeChild(container.firstChild);
+    round = 0;
+}
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', ()=>{
+    resets();
+});
+const scissor = document.querySelector('#scissor');
+scissor.addEventListener('click', () =>{
+    singleRound("scissor", getComputerChoice());
+});
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', ()=>{
+    singleRound('rock', getComputerChoice());
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', ()=>{
+    singleRound('paper',getComputerChoice());
 });
 
 // function game(){
