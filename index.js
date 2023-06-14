eec82df05edbb62db6fd7af55c0c582d80d7b055
ChrisRamFrontEnd <pheonixking56885@gmail.com>
2023-06-14 14:48:50 -0400

    changes to html

--- index.js
+++ index.js
@@ -25,29 +25,31 @@ function singleRound(playerSelection, computerSelection){
         return 0;
 }
 
-function game(){
-    let wins = 0, 
-    losses  = 0, 
-    ties = 0;
+
+
+// function game(){
+//     let wins = 0, 
+//     losses  = 0, 
+//     ties = 0;
     
-    for(let i = 0; i < 5; i++){
-        let selection = prompt("Please enter one of the three choices, Rock, Paper, or Scissors");
-        let result = singleRound(selection, getComputerChoice());
-        if(result=== 1){
-            wins++;
-            document.getElementById("wins").innerHTML = `Wins: ${wins}`;
-        }
-        else if(result === 0){
-            losses++;
-            document.getElementById("losses").innerHTML = `Losses: ${losses}`;
-        }
-        else{
-            ties++;
-            document.getElementById("ties").innerHTML = `Ties: ${ties}`;
-        }
-    }
-    wins > losses ? document.getElementById("result").innerHTML = "YOU WON!!" 
-    : wins === losses ? document.getElementById("result").innerHTML = "YOU TIED!"
-    : ties >= wins ? document.getElementById("result").innerHTML = "You tied quite a bit there!"
-    : document.getElementById("result").innerHTML = "YOU LOSE!! :(";
-}
+//     for(let i = 0; i < 5; i++){
+//         let selection = prompt("Please enter one of the three choices, Rock, Paper, or Scissors");
+//         let result = singleRound(selection, getComputerChoice());
+//         if(result=== 1){
+//             wins++;
+//             document.getElementById("wins").innerHTML = `Wins: ${wins}`;
+//         }
+//         else if(result === 0){
+//             losses++;
+//             document.getElementById("losses").innerHTML = `Losses: ${losses}`;
+//         }
+//         else{
+//             ties++;
+//             document.getElementById("ties").innerHTML = `Ties: ${ties}`;
+//         }
+//     }
+//     wins > losses ? document.getElementById("result").innerHTML = "YOU WON!!" 
+//     : wins === losses ? document.getElementById("result").innerHTML = "YOU TIED!"
+//     : ties >= wins ? document.getElementById("result").innerHTML = "You tied quite a bit there!"
+//     : document.getElementById("result").innerHTML = "YOU LOSE!! :(";
+// }
